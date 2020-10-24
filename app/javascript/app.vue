4c776bb02c44260386cfa3087b5f0d5744007a64
松縄竜弥 <ryuya.matsunawa@seattleconsulting.co.jp>
2020-10-24 22:10:36 +0900

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+#app
+  p {{ message }}
+</template>
+
+<script>
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+    const message = ref("Hello Vue!");
+
+    return {
+      message
+    };
+  }
+});
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment
+
+
+const pug = require('./loaders/pug')
+
+environment.loaders.prepend('pug', pug)
